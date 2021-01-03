cf032dd95edeb158779bb85c3c71dfe3ca61b5cb
codabool <codabool@pm.me>
2021-01-03 11:42:08 -0500

    First commit

---
+++ pages/about.js
@@ -0,0 +1,48 @@
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Fade from 'react-reveal/Fade'
+// import Image from 'next/image'
+
+export default function About() {
+  return (
+    <div className="my-5">
+      <Row>
+        <Col lg={6} className="mt-4">
+          <img
+            src="/assets/authorImg/codabool-lg.jpg"
+            alt="me"
+            className="shadow rounded-circle"
+            width={300}
+            height={300}
+          />
+        </Col>
+        <Col lg={6} className="mb-4">
+          <Fade bottom>
+            <h1 className="display-4 m-2">Hello, <div className="sway d-inline-block">👋</div></h1>
+          </Fade>
+          <Fade delay={1000}>
+            <h3 className="m-1">&emsp;I'm newly graduated, an active developer and a devops intern. I work for a company called Ellucian which offers IT solutions for higher education. Please feel free to look around at my projects, blog or reach out to me. Thanks for visiting. </h3>
+          </Fade>
+        </Col>
+      </Row>
+      <br/>
+      <Row>
+        <Col lg={6} className="mb-4">
+          <Fade bottom delay={2000}>
+            <h1 className="display-4 m-2">About me. 💻</h1>
+          </Fade>
+          <Fade delay={3000}>
+            <h3 className="m-1">&emsp;I graduated December of 2020. My Bachelors is in Computing Technology and Software Development. I am a fullstack web developer. I'm currently working for an internship in devops using AWS with Jenkins and Ansible.</h3>
+          </Fade>
+        </Col>
+        <Col lg={6}>
+          <img
+            src="/assets/images/gamejam-group.jpg"
+            alt="group"
+            className="shadow rounded w-100"
+          />
+        </Col>
+      </Row>
+    </div>
+  )
+}
---
+++ pages/projects/index.js
@@ -0,0 +1,105 @@
+import Fade from 'react-reveal/Fade'
+import Badge from 'react-bootstrap/Badge'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
+import { useRouter } from 'next/router'
+
+const ProjectBox = ({ description, link, delay, tags, year, image }) => {
+  const router = useRouter()
+  return (
+    <Col md={6}>
+      <Card className="post my-4 shadow rounded" onClick={() => router.push(link)}>
+        <Card.Img src={image} />
+        <Fade delay={delay}>
+          <Card.Body>
+            <h4>{description}</h4>
+          </Card.Body>
+        </Fade>
+        <Card.Footer>
+          <small className="mr-2">{year}</small>
+          {tags && tags.map(tag => (
+            <Badge pill variant="info" key={tag} className="ml-2">
+              {tag}
+            </Badge>
+          ))}
+        </Card.Footer>
+      </Card>
+    </Col>
+  )
+}
+
+export default function Projects() {
+  return (
+    <div className="my-5">
+      <h1 className="display-2 m-4">My Projects. 🔨</h1>
+      <hr />
+      <Row>
+        <ProjectBox
+          description="Machine Learning search engine for internal company systems. Using ElasticSearch with current data loaded from several systems. This search engine will provide links and short descriptions similar to Google."
+          link="/projects/8"
+          tags={['React', 'Express', 'ElasticSearch', 'Python']}
+          delay={0}
+          year={2020}
+          image="/assets/coverImg/react-search.jpg"
+        />
+        <ProjectBox
+          description="Fullstack Nextjs e-commerce project. My most complete project yet. Connects to Stripe and a PostgresQL database to allow for products to be added to cart and checked out. Many different tools involved here. Check it out!"
+          link="/projects/7"
+          tags={['Nextjs', 'Express', 'Stripe', 'PostgresQL']}
+          delay={0}
+          year={2020}
+          image="/assets/coverImg/market.jpg"
+        />
+        <ProjectBox
+          description="Senior fullstack project to assist with spotting human trafficking and reporting it. This is a social media site to make posts with images and make comments about missing people."
+          link="/projects/6"
+          tags={['React', 'Express', 'MongoDB', 'AWS S3']}
+          delay={0}
+          year={2020}
+          image="/assets/coverImg/traffick.jpg"
+        />
+        <ProjectBox
+          description="React Dashboard. Sample of a currently used production page created in my intern postion at Ellucian"
+          link="/projects/5"
+          tags={['React']}
+          delay={0}
+          year={2020}
+          image="/assets/coverImg/ecrm-dashboard.jpg"
+        />
+        <ProjectBox
+          description="Django Social Media site using Bootstrap, Misaka and django-braces. Hosted using Heroku"
+          link="/projects/4"
+          tags={['Django']}
+          delay={600}
+          year={2019}
+          image="/assets/coverImg/dj-social-cover.jpg"
+        />
+        <ProjectBox
+          description="Django Blog using Bootstrap. Hosted using Heroku"
+          link="/projects/3"
+          tags={['Django']}
+          delay={400}
+          year={2019}
+          image="/assets/coverImg/dj-blog-cover.jpg"
+        />
+        <ProjectBox
+          description="Mock Tech Support website with JavaScript form validation project"
+          link="/projects/2"
+          tags={['JavaScript']}
+          delay={200}
+          year={2018}
+          image="/assets/coverImg/tech-cover.jpg"
+        />
+        <ProjectBox
+          description="Mock Photography website"
+          link="/projects/1"
+          tags={['JavaScript']}
+          delay={0}
+          year={2015}
+          image="/assets/coverImg/photo-cover.jpg"
+        />
+      </Row>
+    </div>
+  )
+}
